34848fbddab29d914ef764e6642c21d8da57b903
Houssein Msallem <houssein.msallem@gmail.com>
2025-05-09 13:38:34 +0100

    Made very simple skeleton for dashboard

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,7 +1,15 @@
+import { Anchor, AppShell } from '@mantine/core';
 import React from 'react';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <Anchor href="/dashboard/home">Dashboard</Anchor>
+      <Anchor href="/dashboard/events">Events</Anchor>
+      <Anchor href="/dashboard/addEvent">Add Event</Anchor>
+      <Anchor href="/dashboard/deleteEvent">Delete Event</Anchor>
+    </>
+  );
 };
 
 export default NavBar;
---
+++ src/app/dashboard/deleteEvent/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const deleteEvent = () => {
+  return <div>deleteEvent</div>;
+};
+
+export default deleteEvent;
