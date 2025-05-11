24d4008f97a242c004845bd2602e5064553767a7
Houssein Msallem <houssein.msallem@gmail.com>
2025-05-11 06:47:08 +0100

    made server actioin for form submission

--- src/app/_components/EventForm.tsx
+++ src/app/_components/EventForm.tsx
@@ -1,5 +1,7 @@
+'use client';
 import { Button, Fieldset, Group, Textarea, TextInput } from '@mantine/core';
 import React, { useState } from 'react';
+import { createEvent } from '../actions';
 
 const EventForm = () => {
   const [formData, setFormData] = useState({
@@ -17,89 +19,78 @@ const EventForm = () => {
   ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
 
-    const res = await fetch('http://localhost:3001/event', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-
-    const result = await res.json();
-    console.log('Submitted:', result);
-  };
   return (
-    <Fieldset legend="Submit Event" onSubmit={handleSubmit}>
-      <TextInput
-        label="Event Name"
-        placeholder="Your name"
-        value={formData.name}
-        onChange={handleChange}
-      />
-      <Group>
+    <Fieldset legend="Submit Event">
+      <form action={createEvent}>
         <TextInput
-          label="City"
-          placeholder="City"
-          mt="md"
-          value={formData.city}
+          label="Event Name"
+          placeholder="Event Name"
+          value={formData.name}
           onChange={handleChange}
         />
-        <TextInput
-          label="Location"
-          placeholder="Location"
-          mt="md"
-          value={formData.location}
+        <Group>
+          <TextInput
+            label="City"
+            placeholder="City"
+            mt="md"
+            value={formData.city}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Location"
+            placeholder="Location"
+            mt="md"
+            value={formData.location}
+            onChange={handleChange}
+          />
+        </Group>
+        <Group>
+          <TextInput
+            label="Date"
+            placeholder="Date"
+            mt="md"
+            value={formData.date}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Duration"
+            placeholder="Duration"
+            mt="md"
+            value={formData.duration}
+            onChange={handleChange}
+          />
+        </Group>
+        <Group>
+          <TextInput
+            label="Organisation"
+            placeholder="Organisation"
+            mt="md"
+            value={formData.organisation}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Category"
+            placeholder="Category"
+            mt="md"
+            value={formData.category}
+            onChange={handleChange}
+          />
+        </Group>
+        <Textarea
+          label="Description"
+          description="Event Description"
+          placeholder="Event Description"
+          autosize
+          minRows={4}
+          maxRows={8}
+          value={formData.description}
           onChange={handleChange}
         />
-      </Group>
-      <Group>
-        <TextInput
-          label="Date"
-          placeholder="Date"
-          mt="md"
-          value={formData.date}
-          onChange={handleChange}
-        />
-        <TextInput
-          label="Duration"
-          placeholder="Duration"
-          mt="md"
-          value={formData.duration}
-          onChange={handleChange}
-        />
-      </Group>
-      <Group>
-        <TextInput
-          label="Organisation"
-          placeholder="Organisation"
-          mt="md"
-          value={formData.organisation}
-          onChange={handleChange}
-        />
-        <TextInput
-          label="Category"
-          placeholder="Category"
-          mt="md"
-          value={formData.category}
-          onChange={handleChange}
-        />
-      </Group>
-      <Textarea
-        label="Description"
-        description="Event Description"
-        placeholder="Event Description"
-        autosize
-        minRows={4}
-        maxRows={8}
-        value={formData.description}
-        onChange={handleChange}
-      />
-      <Button variant="filled" type="submit">
-        Submit
-      </Button>
+        <Button variant="filled" type="submit">
+          Submit
+        </Button>
+      </form>
     </Fieldset>
   );
 };
---
+++ src/app/actions.ts
@@ -0,0 +1,22 @@
+'use server';
+export async function createEvent(formData: FormData) {
+  const data = {
+    name: formData.get('name'),
+    description: formData.get('description'),
+    city: formData.get('city'),
+    location: formData.get('location'),
+    date: formData.get('date'),
+    duration: formData.get('duration'),
+    organisation: formData.get('organisation'),
+    category: formData.get('category'),
+  };
+  const res = await fetch('http://localhost:3001/event', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await res.json();
+  console.log('Submitted:', result);
+}
