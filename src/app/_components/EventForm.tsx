0724900483b623cf88575c3b3effa621364316c2
Houssein Msallem <houssein.msallem@gmail.com>
2025-05-11 06:02:37 +0100

    added form submission

--- src/app/_components/EventForm.tsx
+++ src/app/_components/EventForm.tsx
@@ -1,21 +1,91 @@
 import { Button, Fieldset, Group, Textarea, TextInput } from '@mantine/core';
-import React from 'react';
+import React, { useState } from 'react';
 
 const EventForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    city: '',
+    location: '',
+    date: '',
+    duration: '',
+    organisation: '',
+    category: '',
+  });
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch('http://localhost:3001/event', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const result = await res.json();
+    console.log('Submitted:', result);
+  };
   return (
-    <Fieldset legend="Submit Event">
-      <TextInput label="Event Name" placeholder="Your name" />
+    <Fieldset legend="Submit Event" onSubmit={handleSubmit}>
+      <TextInput
+        label="Event Name"
+        placeholder="Your name"
+        value={formData.name}
+        onChange={handleChange}
+      />
       <Group>
-        <TextInput label="City" placeholder="City" mt="md" />
-        <TextInput label="Location" placeholder="Location" mt="md" />
+        <TextInput
+          label="City"
+          placeholder="City"
+          mt="md"
+          value={formData.city}
+          onChange={handleChange}
+        />
+        <TextInput
+          label="Location"
+          placeholder="Location"
+          mt="md"
+          value={formData.location}
+          onChange={handleChange}
+        />
       </Group>
       <Group>
-        <TextInput label="Date" placeholder="Date" mt="md" />
-        <TextInput label="Duration" placeholder="Duration" mt="md" />
+        <TextInput
+          label="Date"
+          placeholder="Date"
+          mt="md"
+          value={formData.date}
+          onChange={handleChange}
+        />
+        <TextInput
+          label="Duration"
+          placeholder="Duration"
+          mt="md"
+          value={formData.duration}
+          onChange={handleChange}
+        />
       </Group>
       <Group>
-        <TextInput label="Organisation" placeholder="Organisation" mt="md" />
-        <TextInput label="Category" placeholder="Category" mt="md" />
+        <TextInput
+          label="Organisation"
+          placeholder="Organisation"
+          mt="md"
+          value={formData.organisation}
+          onChange={handleChange}
+        />
+        <TextInput
+          label="Category"
+          placeholder="Category"
+          mt="md"
+          value={formData.category}
+          onChange={handleChange}
+        />
       </Group>
       <Textarea
         label="Description"
@@ -24,6 +94,8 @@ const EventForm = () => {
         autosize
         minRows={4}
         maxRows={8}
+        value={formData.description}
+        onChange={handleChange}
       />
       <Button variant="filled" type="submit">
         Submit
